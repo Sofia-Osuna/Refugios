14673cbd8ccc85539fc61c42d2860f132c62d77a
Sofia_Osuna <sofiaosuna227@gmail.com>
2025-10-21 22:40:19 -0700

    Intento 2 .... carpetas, bootstrap y hola mundo

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Refugios</title>
+</head>
+<body>
+    <h1>Hola Mundo UwU</h1>
+</body>
+</html>
