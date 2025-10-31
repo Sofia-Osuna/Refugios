f6df6da0bc26b226a3c23b9646b7b869fe8b61b2
pedro <sanchezsandovalpedroantonio@gmail.com>
2025-10-31 09:15:59 -0700

    intento de subir 1

--- index.php
+++ index.php
@@ -3,10 +3,90 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Refugios</title>
+    <title>Menu principal</title>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        h1{
+            text-align: center;
+        }
+
+        
+    </style>
+
+   
+
 </head>
 <body>
-    <h1>Hola Mundo UwU</h1>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Menu</a>
+
+ 
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Buscar refugios" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Buscar</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<br>
+<h1>¡RefuPets!</h1>
+<br><br><br><br>
+<figure>
+   <img src="Imagenes_animales/gatos.webp"> 
+   <figcaption>
+       ¡Encuentra a tu compañero ideal!
+   </figcaption>
+  </figure>
+  <br><br>
+
+  <h2>¿Como funciona?</h2>
+  <br>
+  <i>En RefuPet podras encontrar refugios para mascotas ubicados en toda la republica, Dentro de cada refugio podrás ver la amplia selección de mascotas disponibles para adoptar, ¡Será imposible no encontrar a un compañero que se adapte a tí!</i>
+  <br><br><br><br><br>
+
+  <h2>Estos amigos necesitan un hogar</h2>
+  <br><br>
+
+  <div>
+        <div>
+            <p>aquí va foto de una mascota registrada, se supone que es dinámico</p>
+            
+        </div>
+        
+        <h2>nombre</h2>
+        <p>Descripción</p>
+        
+        <button>
+            Ver más información
+        </button>
+    </div>
+
+    <div>
+        <div>
+            <p>aquí va foto de una mascota registrada, se supone que es dinámico</p>
+            
+        </div>
+        
+        <h2>nombre</h2>
+        <p>Descripción</p>
+        
+        <button>
+            Ver más información
+        </button>
+    </div>
+
+
+
     
+
+
+
+
+
 </body>
 </html>
