a3f12675c3499d6a31d38bb683dc85daf7ed5b43
Sofia-Osuna <sofiaosuna224@gmail.com>
2025-11-06 09:08:56 -0700

    solo hice el archivo de estilo xdxd

---
+++ menu.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>menu</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    
+    <a class="navbar-brand" href="#">
+        <img src="" alt="">
+        RefuPETS
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item"><a class="nav-link" href="#">Iniciar sesion</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Adoptar</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Refugios</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Contacto</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</body>
+</html>
