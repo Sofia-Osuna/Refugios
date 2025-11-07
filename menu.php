ffed44d8f2fcd649fe9f5798b8bc42202fd25669
Sofia-Osuna <sofiaosuna224@gmail.com>
2025-11-06 22:00:30 -0700

    archivo de menu, el cual se debe de incluir en TODAS las paginas, tambien agregue los dos logos, y pues la case para el menu en estilos

--- menu.php
+++ menu.php
@@ -5,13 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>menu</title>
     <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <!-- Poner otra imagen, no se con un corazon o algo asi -->
+    <link rel="icon" type="image/png" href="img_sistema/logo_color.png"> 
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class=" navbar navbar-expand-lg navbar-light  navbar-custom">
   <div class="container-fluid">
     
     <a class="navbar-brand" href="#">
-        <img src="" alt="">
+        <img  src="img_sistema/logo.png" alt="logo" class="d-inline-block align-text-top me-2" style="height: 60px" >
         RefuPETS
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
@@ -21,8 +24,8 @@
       <ul class="navbar-nav ms-auto">
         <li class="nav-item"><a class="nav-link" href="#">Iniciar sesion</a></li>
         <li class="nav-item"><a class="nav-link" href="#">Adoptar</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">Refugios</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">Contacto</a></li>
+        <li class="nav-item"><a class="nav-link" href="Lista_refugio.php">Refugios</a></li>
+        <li class="nav-item"><a class="nav-link" href="#"></a></li>
       </ul>
     </div>
   </div>
