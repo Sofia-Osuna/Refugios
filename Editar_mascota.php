8020a84205fd8a75183576cda8508faf09d7010f
Kyouma <brayanmartinlopezflores@gmail.com>
2025-11-10 09:54:18 -0700

    oye ola hice para que se pueda agregar foto a las mascotas y que se vean en la lista

--- Editar_mascota.php
+++ Editar_mascota.php
@@ -18,27 +18,31 @@ $especies = $mascota_obj->obtenerEspecies();
 <body>
     <h1>Editar Mascota</h1>
     
-    <form action="controladores/actualizar_mascota.php" method="POST">
-        <input type="hidden" name="id_mascota" value="<?= $mascota['id_mascotas'] ?>">
-        <input type="hidden" name="id_refugio" value="<?= $id_refugio ?>">
-        
-        Nombre:<br>
-        <input type="text" name="nombre" value="<?= $mascota['nombre'] ?>" required><br><br>
-        
-        Descripción:<br>
-        <textarea name="descripcion" required><?= $mascota['descripcion'] ?></textarea><br><br>
-        
-        Especie:<br>
-        <select name="fk_especie" required>
-            <?php foreach($especies as $especie){ ?>
-                <option value="<?= $especie['id_especie'] ?>" <?= $especie['id_especie'] == $mascota['fk_especie'] ? 'selected' : '' ?>>
-                    <?= $especie['nombre'] ?>
-                </option>
-            <?php } ?>
-        </select><br><br>
-        
-        <button type="submit">Actualizar</button>
-        <a href="Lista_mascota.php?id_refugio=<?= $id_refugio ?>">Cancelar</a>
-    </form>
+  <form action="controladores/actualizar_mascota.php" method="POST" enctype="multipart/form-data">
+    <input type="hidden" name="id_mascota" value="<?= $mascota['id_mascotas'] ?>">
+    <input type="hidden" name="id_refugio" value="<?= $id_refugio ?>">
+    
+    Nombre:<br>
+    <input type="text" name="nombre" value="<?= $mascota['nombre'] ?>" required><br><br>
+    
+    Descripción:<br>
+    <textarea name="descripcion" required><?= $mascota['descripcion'] ?></textarea><br><br>
+    
+    Foto actual: <?= $mascota['foto'] ?><br>
+    Nueva foto (opcional):<br>
+    <input type="file" name="foto"><br><br>
+    
+    Especie:<br>
+    <select name="fk_especie" required>
+        <?php foreach($especies as $especie){ ?>
+            <option value="<?= $especie['id_especie'] ?>" <?= $especie['id_especie'] == $mascota['fk_especie'] ? 'selected' : '' ?>>
+                <?= $especie['nombre'] ?>
+            </option>
+        <?php } ?>
+    </select><br><br>
+    
+    <button type="submit">Actualizar</button>
+    <a href="Lista_mascota.php?id_refugio=<?= $id_refugio ?>">Cancelar</a>
+</form>
 </body>
 </html>
--- controladores/Insertar_mascota.php
+++ controladores/Insertar_mascota.php
@@ -1,4 +1,4 @@
-php<?php
+<?php
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
 
@@ -6,10 +6,21 @@ php<?php
     $descripcion = $_POST["descripcion"];
     $fk_especie = $_POST["fk_especie"];
     $fk_refugio = $_POST["fk_refugio"];
+    
+    // Manejar la foto
+    $foto = $_FILES["foto"]["name"];
+    $tmp = $_FILES["foto"]["tmp_name"];
+    
+    if($foto != ""){
+        $ruta = "../imagenes_animales/" . $foto;
+        move_uploaded_file($tmp, $ruta);
+    } else {
+        $foto = "sin_foto.jpg"; // Imagen por defecto
+    }
 
     include ('../clases/Mascota.php');
     $clase = new Mascota();
-    $resultado = $clase->guardar($nombre, $descripcion, $fk_especie, $fk_refugio);
+    $resultado = $clase->guardar($nombre, $descripcion, $foto, $fk_especie, $fk_refugio);
 
     if($resultado){
         header('location: ../Lista_mascota.php?id_refugio=' . $fk_refugio);
