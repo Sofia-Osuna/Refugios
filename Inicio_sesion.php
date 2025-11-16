3a35809bcd40174dc7700482f0968158f67c8ebf
lorena22 <lorena22cardenas@email.com>
2025-11-15 23:10:45 -0700

    Inicio_sesion y Lista_mascotas

--- Inicio_sesion.php
+++ Inicio_sesion.php
@@ -3,62 +3,108 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<br><br>
 	<title>Inicio de sesion</title>
-
 
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
 
 	<style>
 
+		body{
+			font-family: Arial;
+		}
+
 		h1{
 			text-align: center;
+			margin-top: 20px;
+		}
+
+		.contenedor-imagen{
+			text-align: center;
+			margin-top: 20px;
+		}
+
+		.contenedor-imagen img{
+			width: 150px;
+			height: auto;
+			border-radius: 10px;
 		}
 
 		label{
-			text-align: center;
+			text-align: left;
 			display: block;
+			margin-left: 35%;
+			margin-top: 10px;
+			font-weight: bold;
 		}
 
-		input {
+		input[type="text"],
+		input[type="password"]{
             margin: 0 auto;
             display: block;
+            width: 30%;
+            padding: 8px;
+            border-radius: 15px;
+            border: 1px solid #ccc;
+        }
+
+        input[type="submit"]{
+        	display: block;
+        	margin: 20px auto;
+        	padding: 10px 20px;
+        	border: none;
+        	border-radius: 20px;
+        	background-color: #FFC107;
+        	color: black;
+        	font-weight: bold;
+        	cursor: pointer;
+        	width: 10%;
+        }
+
+        input[type="submit"]:hover{
+        	background-color: #e0a800;
         }
 
         h5{
         	text-align: center;
+        	margin-top: 30px;
         }
 
-        a{
+        .registrate{
         	display: block;
         	text-align: center;
+        	margin-top: 5px;
+        	font-weight: bold;
+        	color: #28A745;
+        	text-decoration: none;
+        	font-size: 18px;
         }
 
-		
+        .registrate:hover{
+        	color: #1e7e34;
+        }
+
 	</style>
 
 </head>
 <body>
+
+<div class="contenedor-imagen">
+	<img src="https://img.freepik.com/vector-premium/amor-perro-icono-corazon-dentro-perro-pata-vector-icono_654297-339.jpg?w=1380">
+</div>
+
 <h1>¡Bienvenido!</h1>
-<br>
 
 <label>Nombre de usuario</label>
-<br>
-
 <input type="text" name="usuario">
-<br>
 
 <label>Contraseña</label>
-<br>
-
 <input type="password" name="contraseña">
-<br>
 
-<input type="submit" value="Iniciar sesion" name="Iniciar sesion">
-<br><br>
+<input type="submit" value="Iniciar sesion">
 
 <h5>¿No tienes cuenta?</h5>
 
-<a>Crea tu cuenta aqui</a>
+<a href="#" class="registrate">Regístrate aquí</a>
+
 </body>
 </html>
